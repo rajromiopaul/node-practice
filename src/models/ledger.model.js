57b04b34c7788a8abf70aa175aaa959bfb25d6b2
Raj Romio Paul <rajromiopaul@gmail.com>
2026-07-29 22:03:08 +0530

    transaction model created

---
+++ src/models/ledger.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const ledgerSchema = new mongoose.Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'account',
+        required: [true, 'Ledger must be associated with an account'],
+        immutable: true
+    },
+    amount: {
+        type: Number,
+        required: [true, "Amount is required for creating a ledger entry"],
+        immutable: true
+    },
+    transaction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'transaction',
+        required: [true, "Ledger must be associated with a transaction"],
+        index: true,
+        immutable: true
+    },
+    type: {
+        type: String,
+        enum: {
+            values: ['CREDIT', 'DEBIOT'],
+            message: "Type can be either CREDIT or DEBIT", \
+        },
+        required: [true, "Ledger type is required"],
+        immutable: true
+    }
+})
